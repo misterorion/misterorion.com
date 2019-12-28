1491035ceabef9b601bb855daa0a407b11d081e0
Orion Anderson <orion@mechapower.com>
2019-12-27 19:12:42 -0500

    Add needed plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,58 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
+    `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/content`,
-      },
+        name: `images`,
+        path: `${__dirname}/content/images`
+      }
     },
-    `gatsby-transformer-remark`
-  ],
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js")
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/posts`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/content/pages`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-embed-video`,
+          `gatsby-remark-responsive-iframe`,
+          `gatsby-remark-smartypants`,
+          {
+            resolve: `gatsby-remark-prismjs`
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+              showCaptions: true,
+              tracedSVG: true
+            }
+          }
+        ]
+      }
+    }
+  ]
 }
