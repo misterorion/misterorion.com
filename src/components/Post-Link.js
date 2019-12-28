fdfd84ba92c4ed5b1d8b621b5ae1483ab014fb32
Orion Anderson <orion@mechapower.com>
2019-12-28 12:14:47 -0500

    Bolder hover color.

--- src/components/Post-Link.js
+++ src/components/Post-Link.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 const PostLink = ({ post }) => (
   <Link to={post.frontmatter.slug}>
-    <div className="group w-auto mb-6 p-6 border border-gray-400 hover:border-teal-400 rounded shadow">
+    <div className="group w-auto mb-6 p-6 border border-gray-400 hover:border-red-500 rounded shadow">
       <div className="text-2xl text-gray-900 font-bold">{post.frontmatter.title}</div>
       <div className="text-gray-600">{post.frontmatter.date}</div>
     </div>
