960ad31a36a5d613aefd1156f8582929c8ada6b8
Orion Anderson <orion@mechapower.com>
2019-12-28 12:15:02 -0500

    Simplify view on mobile.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,7 @@ export default ({ children }) => {
   return (
     <>
     <Metadata />
-    <div class="container rounded px-6 py-6 my-3 sm:w-5/6 md:px-32 lg:w-4/5 max-w-5xl bg-gray-100 border border-gray-400">
+    <div class="container rounded px-6 py-6 sm:my-3 w-full sm:w-5/6 md:px-32 lg:w-4/5 max-w-5xl bg-gray-100 sm:border border-gray-400">
       <Navigation />
       <article>{children}</article>
       <Footer data={SiteConfig} />
