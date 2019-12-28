110d4049e2157f9a2a1dd7caec92b29f128258c6
Orion Anderson <orion@mechapower.com>
2019-12-28 12:14:07 -0500

    Add GA; export as Node module.

--- SiteConfig.js
+++ SiteConfig.js
@@ -1,9 +1,11 @@
-export default {
+const config = {
   siteTitle: "Orion Anderson",
   siteURL: "https://misterorion.com",
-  userName: "Orion",
+  userName: "Orion Anderson",
   userEmail: "orion@mechapower.com",
   userTwitter: "MisterOrion",
   userLinkedIn: "orionanderson",
-  userGitHub: "MisterOrion"
+  userGitHub: "MisterOrion",
+  googleAnalyticsID: "UA-27884057-5"
 };
+module.exports = config
