cb74fda22659f1d402d5fa667b3b908287b2c7cf
Orion Anderson <orion@mechapower.com>
2019-12-27 19:45:52 -0500

    Add plugin manifest.

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,20 @@ module.exports = {
           }
         ]
       }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Orion Anderson`,
+        short_name: `Orion Anderson`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `content/images/favicon.png` // This path is relative to the root of the site.
+      }
     }
   ]
 }
