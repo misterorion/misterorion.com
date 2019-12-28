d8e866a5b899127d9d555548690686cf1368b58a
Orion Anderson <orion@mechapower.com>
2019-12-27 19:14:38 -0500

    Add page and post templates.

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,32 +1,56 @@
-import React from "react";
-import { graphql } from "gatsby";
-export default function Template({
-  data // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data; // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark;
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Img from "gatsby-image"
+
+export default ({ data }) => {
+  const { markdownRemark: post } = data
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
-  );
+    <Layout>
+      <h1 className="text-4xl mb-4 mt-12 font-bold">
+        {post.frontmatter.title}
+      </h1>
+      <div className="text-gray-600 mb-10">{post.frontmatter.date}</div>
+
+      {/* Check for featured image */}
+
+      {post.frontmatter.imageFluid ? (
+        <div className="my-20">
+          <Img fluid={post.frontmatter.imageFluid.childImageSharp.fluid} />
+        </div>
+      ) : (
+        <div className="my-20">
+          <Img fixed={post.frontmatter.imageFixed.childImageSharp.fixed} />
+        </div>
+      )}
+
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </Layout>
+  )
 }
 export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
+  query BlogPostByPath($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       html
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
-        path
+        slug
         title
+        imageFluid {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        imageFixed {
+          childImageSharp {
+            fixed(width:200 ) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
       }
     }
   }
-`;
+`
