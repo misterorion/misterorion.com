248e9f0dd3ec960e5b3ec39a1b64d6960e345f33
Orion Anderson <orion@mechapower.com>
2019-12-28 21:39:59 -0500

    Better responsive sizing

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,10 +4,10 @@ import Me from "../../content/images/me.jpg"
 export default () => {
   return (
     <div className="flex w-auto mb-8 p-6 items-center border border-teal-400 rounded shadow">
-      <div className="block w-full flex-auto sm:w-1/3 lg:w-1/4 mr-6" >
-        <img src={Me} alt="Orion Anderson" className="w-40"/>
+      <div className="w-1/4 sm:w-1/3 lg:w-1/4 mr-6" >
+        <img src={Me} alt="Orion Anderson" />
       </div>
-        <div className="block w-full sm:w-2/3 lg:w-3/4 text-lg sm:text-lg lg:text-xl xl:text-2xl leading-snug">
+        <div className="w-3/4 sm:w-2/3 lg:w-3/4 text-base sm:text-base lg:text-xl xl:text-2xl leading-snug">
           Greetings! My name is Orion Anderson. I'm a DevOps Developer located in New York City. I love all things Linux, Docker, AWS and automation. I also dabble in
           front-end development.
         </div>
--- src/components/Post-Link.js
+++ src/components/Post-Link.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 export default ({ post }) => (
   <Link to={`/${post.frontmatter.slug}/`}>
     <div className="mb-6 p-6 border border-gray-400 hover:border-red-500 hover:bg-orange-100 rounded hover:shadow">
-      <div className="text-2xl text-gray-900 font-bold">{post.frontmatter.title}</div>
+      <div className="text-xl sm:text-2xl text-gray-900 font-bold">{post.frontmatter.title}</div>
       <div className="text-gray-600">{post.frontmatter.date}</div>
     </div>
   </Link>
