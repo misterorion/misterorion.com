f420d0bf50879be2a96d1fc11fd1d90f9d3eae79
Orion Anderson <orion@mechapower.com>
2020-01-04 19:34:11 -0500

    We can clean up variables

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,27 +5,27 @@ import Img from "gatsby-image"
 import Helmet from "react-helmet"
 
 export default ({ data }) => {
-  const { markdownRemark: post } = data
+  const { frontmatter: post } = data.markdownRemark
+  const { html: content } = data.markdownRemark
   const _ = require("lodash")
-  
-  // Check for featured image in frontmatter
-  const featImg = post.frontmatter.imageFluid ? (
-    <Img fluid={post.frontmatter.imageFluid.childImageSharp.fluid} />
+
+  const image = post.imageFluid ? (
+    <Img fluid={post.imageFluid.childImageSharp.fluid} />
   ) : (
-    <Img fixed={post.frontmatter.imageFixed.childImageSharp.fixed} />
+    <Img fixed={post.imageFixed.childImageSharp.fixed} />
   )
 
   return (
     <Layout>
-      <Helmet title={post.frontmatter.title} />
-      <h1>{post.frontmatter.title}</h1>
-      <div className="post-date">{post.frontmatter.date}</div>
-      <div className="feat-img">{featImg}</div>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <Helmet title={post.title} />
+      <h1>{post.title}</h1>
+      <div className="post-date">{post.date}</div>
+      <div className="feat-img">{image}</div>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
       <div className="tags border-t mt-6 pt-6 border-gray-400">
         <h3 className="inline">Tags:</h3>
         <ul className="inline ml-2">
-          {post.frontmatter.tags.map(tag => (
+          {post.tags.map(tag => (
             <li className="inline-block mx-2">
               <Link to={`/tags/${_.kebabCase(tag)}`}>{tag}</Link>
             </li>
