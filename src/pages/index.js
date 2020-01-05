33c927a54bce0b5deaca92d24648778cffc4fd41
Orion Anderson <orion@mechapower.com>
2020-01-04 19:15:36 -0500

    Simplify

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,21 @@ import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import PostLink from "../components/Post-Link"
 
-const IndexPage = ({
-  data: {
-    posts: { edges }
-  }
-}) => {
-  const Posts = edges.map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+export default ({ data }) => {
+  const Posts = data.allMarkdownRemark.edges.map(edge => (
+    <PostLink key={edge.node.id} post={edge.node} />
+  ))
   return (
     <Layout>
-    <Hero />
+      <Hero />
       <div className="py-6">{Posts}</div>
     </Layout>
   )
 }
-export default IndexPage
 
 export const pageQuery = graphql`
   query MyQuery {
-    posts: allMarkdownRemark(
+    allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date] }
       filter: { fileAbsolutePath: { regex: "/(posts)/" } }
     ) {
