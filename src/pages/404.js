872ba69cfe2fc47ce13685270451e0b0421da7b7
Orion Anderson <orion@mechapower.com>
2020-01-05 00:25:55 -0500

    Remove unused styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout"
 export default () => {
   return (
     <Layout>
-      <h1 className="text-4xl mb-4 mt-12 font-bold">404 Not Found.</h1>
+      <h1>404 Not Found.</h1>
     </Layout>
   )
 }
