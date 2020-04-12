5d8990119bf63786f5475f6647785125ec1e8cd9
Orion Anderson <orion@mechapower.com>
2020-04-12 17:51:53 -0400

    Importing named value for Helmet 6.0.0

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 import Layout from "../components/Layout"
 import PostLink from "../components/Post-Link"
-import Helmet from "react-helmet"
 
 export default ({ pageContext, data }) => {
   const { tag } = pageContext
