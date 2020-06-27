82a6bfff7bd55fee3f4afac6183c241372e57e04
Orion Anderson <orion@mechapower.com>
2020-06-27 19:09:12 -0400

    Remove trailing slashes

--- src/components/Post-Link.js
+++ src/components/Post-Link.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import PostStyles from "./style/post-link.module.css"
 
 export default ({ post }) => (
-  <Link to={`/${post.frontmatter.slug}/`}>
+  <Link to={`/${post.frontmatter.slug}`}>
     <div className={PostStyles.box}>
       <div className={PostStyles.title}>{post.frontmatter.title}</div>
       <div className={PostStyles.date}>{post.frontmatter.date}</div>
