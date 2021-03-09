b1e73bbd1ce168d638f830491d0e4d258ff65cd8
Orion Anderson <orion@mechapower.com>
2021-03-09 00:59:04 -0500

    Update monospace font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,14 @@ import React from 'react'
 import SEO from '../components/SEO'
 import Navigation from './Navigation'
 import Footer from './Footer'
-import "@fontsource/roboto/400.css"
-import "@fontsource/roboto/400-italic.css"
-import "@fontsource/roboto/700.css"
-import "@fontsource/quicksand/700.css"
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/400-italic.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/quicksand/700.css'
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/400-italic.css'
+import '@fontsource/jetbrains-mono/600.css'
+import '@fontsource/jetbrains-mono/600-italic.css'
 
 const Layout = ({ children }) => {
   return (
