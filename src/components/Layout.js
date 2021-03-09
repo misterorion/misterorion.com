795bc0dcebb10b2e85ac6c44a71231a75da2e875
Orion Anderson <orion@mechapower.com>
2021-03-09 00:16:33 -0500

    Add local fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,10 @@ import React from 'react'
 import SEO from '../components/SEO'
 import Navigation from './Navigation'
 import Footer from './Footer'
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/400-italic.css"
+import "@fontsource/roboto/700.css"
+import "@fontsource/quicksand/700.css"
 
 const Layout = ({ children }) => {
   return (
