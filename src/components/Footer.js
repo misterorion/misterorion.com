b1e9055dac7fa723416f793037c552f1b04dc619
Orion Anderson <orion@mechapower.com>
2021-08-13 19:31:11 -0400

    Consolidate imports

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import { icon } from './modules/footer.module.css'
-import { container } from './modules/footer.module.css'
+import { container, icon } from './modules/footer.module.css'
 import TwitterLogo from './icons/twitter.svg'
 import GithubLogo from './icons/github.svg'
 import LinkedInLogo from './icons/linkedin.svg'
