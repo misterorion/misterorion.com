1797f9baca83f55f701da54e62929a5fff88570b
Orion Anderson <orion@mechapower.com>
2022-02-04 01:13:30 -0500

    Tweak 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import { Helmet } from "react-helmet"
 import Layout from '../components/Layout'
 
 const Error = () => {
   return (
     <Layout>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Orion Anderson - 404</title>
+      </Helmet>
       <h1>404 Not Found.</h1>
     </Layout>
   )
