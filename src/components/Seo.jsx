ff9e6d0afed09d973e90c582a896dc0ede3bd992
Orion Anderson <orion@mechapower.com>
2022-11-05 12:46:27 -0400

    Test logic for seo and descriptions

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -17,29 +17,29 @@ const Seo = ({ title, description, image, url }) => {
     return null;
   }
 
-  if (!image) {
-    image = siteImage;
-  }
+  // if (!image) {
+  //   image = siteImage;
+  // }
 
-  if (!description) {
-    description = siteDescription;
-  }
+  // if (!description) {
+  //   description = siteDescription;
+  // }
 
   return (
     <Fragment>
       <title>{title || siteTitle}</title>
       <link rel="canonical" href={url || `${siteUrl}`} />
-      <meta name="description" content={description} />
+      <meta name="description" content={description || siteDescription} />
       <meta name="author" content={userName} />
-      <meta property="og:image" content={`${siteUrl}${image}`} />
-      <meta property="og:description" content={description} />
+      <meta property="og:image" content={`${siteUrl}${image || siteImage}`} />
+      <meta property="og:description" content={description || siteDescription} />
       <meta property="og:title" content={title || siteTitle} />
       <meta property="og:type" content="article" />
       <meta property="og:url" content={url || `${siteUrl}`} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={`@${userTwitter}`} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`${siteUrl}${image}`} />
+      <meta name="twitter:description" content={description || siteDescription} />
+      <meta name="twitter:image" content={`${siteUrl}${image || siteImage}`} />
       <meta name="twitter:title" content={title || siteTitle} />
       <script defer data-domain="misterorion.com" src="/js/script.js"></script>
     </Fragment>
