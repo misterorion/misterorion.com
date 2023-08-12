3a615c919e8ef202ab01e2514126e6bfdf4a6133
Orion Anderson <orion@mechapower.com>
2023-08-12 18:37:07 -0400

    Revert form endpoint

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -16,8 +16,7 @@ import {
   success,
 } from "./ContactForm.module.css";
 
-// const formEndpoint = "https://misterorion.com/contact/";
-const formEndpoint = "https://nhv3tpknrguux46ubb5lkskala0nwvrb.lambda-url.us-east-2.on.aws/contact/"
+const formEndpoint = "https://misterorion.com/contact/";
 
 const ContactForm = () => {
   const [buttonVisible, setButtonVisible] = useState(true);
