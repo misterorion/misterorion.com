053091606270cb82f75668ad6a7d144def3d4c26
Orion Anderson <orion@mechapower.com>
2023-10-30 21:16:33 -0400

    Fix syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,12 +10,36 @@ export default defineConfig({
     shikiConfig: {
       theme: "github-dark-dimmed",
       langs: [
+        'bash',
         {
           id: "caddy",
           scopeName: "source.Caddyfile",
           path: "../../src/caddyfile.tmLanguage.json",
         },
+        'docker',
+        'go',
+        'json',
+        'jsx',
+        'powershell',
+        'python',
+        'sh',
+        'shell',
+        'yaml',
       ],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
     },
   },
+  // markdown: {
+  //   shikiConfig: {
+  //     theme: "github-dark-dimmed",
+  //     langs: [
+  //       {
+  //         id: "caddy",
+  //         scopeName: "source.Caddyfile",
+  //         path: "../../src/caddyfile.tmLanguage.json",
+  //       },
+  //     ],
+  //   },
+  // },
 });
