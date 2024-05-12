d57abfd5ec117a90d42bfb114fa1e8ae25abc213
Orion Anderson <orion@mechapower.com>
2024-05-12 01:17:47 -0400

    Test cloudflare response

---
+++ functions/mgtest.js
@@ -0,0 +1,14 @@
+export default {
+  async fetch(req) {
+    const data =
+      req.cf !== undefined
+        ? req.cf
+        : { error: "The `cf` object is not available inside the preview." };
+
+    return new Response(JSON.stringify(data, null, 2), {
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    });
+  },
+};
