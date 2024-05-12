6ad9d1e0b82c45aeaf0801a1e34445b4c6fe09f0
Orion Anderson <orion@mechapower.com>
2024-05-12 01:44:04 -0400

    Test function

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
   console.log(`[LOGGING FROM /hello]: Request came from ${context.request}`);
 
-  return new Response("Hello, world!");
+  return new Response("Hello, world! Ya jerk!");
 }
